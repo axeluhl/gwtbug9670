a5766e4f548708b96250408a3440c0d7d912574a
Axel Uhl <axel.uhl@sap.com>
2019-10-12 21:04:44 +0200

    sample project to reproduce GWT bug #9670 / #9664

---
+++ gwtbug/src/gwtbug/client/A2.java
@@ -0,0 +1,8 @@
+package gwtbug.client;
+
+public class A2 {
+	public static class C<T> {
+		public void m(final T t) {
+		}
+	}
+}
---
+++ gwtbug/src/gwtbug/client/Gwtbug.java
@@ -0,0 +1,23 @@
+package gwtbug.client;
+
+import java.util.function.Consumer;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Gwtbug implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	    final A2.C<String> instance2 = new A2.C<>();
+	    final Consumer<String> methodReference2 = instance2::m;
+	    final A1.C<String> instance1 = new A1.C<>();
+	    final Consumer<String> methodReference1 = instance1::m;
+	    RootPanel.get().add(new Label("Result: "+methodReference1+", "+methodReference2));
+	}
+}
